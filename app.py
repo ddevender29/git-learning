77c100013db2a143b27b8e592006afbc28b41f3c
ddevender29 <devender.singh@walkingtree.tech>
2026-06-09 12:55:50 +0530

    Testing division

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
+<<<<<<< HEAD
 def greet(name):
     return f"Hello, {name}!"
 
 print(greet("World"))
+=======
+user = input("Enter your name: ")
+
+if not user:
+    user = "Guest"
+
+print(greet(user))
+>>>>>>> 76a567c... Handle empty user input
+
+print(10 / 0)
